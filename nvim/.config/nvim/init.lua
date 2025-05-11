7ecc1a866dc326edf1cf730fb28437db89f4fe6b
Marco van Rooyen <marco.vanrooyen@dull.net>
2025-05-12 07:42:12 +1000

    Adding vscode, wezterm, zshrc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,9 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+  require("_vscode.options")
+  require("_vscode.keymaps")
+  require("_vscode.lazy")
+else
+  -- ordinary Neovim
+  require("config.lazy")
+end
